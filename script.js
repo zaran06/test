1199b6d045b693dcdacd53a2472bb9c30cbf3081
zaran06 <ruslan.zarbaliev63@gmail.com>
2026-02-17 14:51:20 +0400

    added punctuation marks and corrected tabs

--- script.js
+++ script.js
@@ -5,10 +5,10 @@
 
   recolorFirstCardButton.addEventListener('click', () => {
     firstProductCard.style.backgroundColor=greenHashColor;
-  })
+});
 
   // покраска всех карточек
-  const blueHashColor ='#6fbaec'
+  const blueHashColor ='#6fbaec';
   const productCards = document.querySelectorAll('.product-card');
   const recolorAllCardButton = document.getElementById('recolor-all-card-button');
 
@@ -16,29 +16,27 @@
   productCards.forEach(card => {
     card.style.backgroundColor = blueHashColor;
   });
-})
+});
 
   // Переход на страницу Google 
   const googleOpenButton = document.getElementById('open-google-button');
   googleOpenButton.addEventListener('click', openGoogle);
 
   function openGoogle() {
-    const answer = confirm('Вы действительно хотите перейти на сайт Google?') 
+    const answer = confirm('Вы действительно хотите перейти на сайт Google?');
     if (answer === true) {
       window.open('https://google.com');
-    } else {
-
   }
 }
 
   // Вывод контента элемента в консоль 
-  const mainTitle = document.querySelector('.main-title')
+  const mainTitle = document.querySelector('.main-title');
   mainTitle.addEventListener('mouseenter', () => {
-    console.log(mainTitle.textContent)
+    console.log(mainTitle.textContent);
   });
 
   // Добавление новой кнопки которая меняет цвет при нажатии
   const changeTheColorOfTheButton = document.getElementById('change-the-color-of-the-button');
   changeTheColorOfTheButton.addEventListener('click', () => {
-    changeTheColorOfTheButton.classList.toggle ('active')
-})
+    changeTheColorOfTheButton.classList.toggle('active');
+});
