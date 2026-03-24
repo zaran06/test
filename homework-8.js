74c41cc3211bbd41e9a7a0b26b01f5bae95975a1
zaran06 <ruslan.zarbaliev63@gmail.com>
2026-03-24 06:00:08 +0400

    Fixed tabulation, fixed errors in the code, closed and made tags in one line

--- homework-8.js
+++ homework-8.js
@@ -1,35 +1,36 @@
 import { productCards } from "./product-cards.js";
-// 3. Задание: Создание и реализование шаблона для продуктовых карточек.
-function renderCards(cardsArray) {
+
 const productTemplate = document.getElementById("product-template");
 const container = document.querySelector(".container");
 
-if (!container || !productTemplate) return;
+// 3. Задание: Создание и реализование шаблона для продуктовых карточек.
+function renderCards(cardsArray) {
+  if (!container || !productTemplate) return;
 
-container.innerHTML = "";
+  container.innerHTML = "";
 
-cardsArray.forEach(productCard => {
-  const productCardClone = productTemplate.content.cloneNode(true);
+  cardsArray.forEach(productCard => {
+    const productCardClone = productTemplate.content.cloneNode(true);
 
-  productCardClone.querySelector(".card-title").textContent = productCard.title;
-  productCardClone.querySelector(".card-category").textContent = productCard.skinType;
-  productCardClone.querySelector(".card-description").textContent = productCard.description;
-  productCardClone.querySelector(".card-image").src = `images/${productCard.image}.png`;
-  productCardClone.querySelector(".price-value").textContent = `${productCard.price} руб.`;
+    productCardClone.querySelector(".card-title").textContent = productCard.title;
+    productCardClone.querySelector(".card-category").textContent = productCard.skinType;
+    productCardClone.querySelector(".card-description").textContent = productCard.description;
+    productCardClone.querySelector(".card-image").src = `images/${productCard.image}.png`;
+    productCardClone.querySelector(".price-value").textContent = `${productCard.price} руб.`;
 
-  const compositionList = productCardClone.querySelector(".composition-list");
-  productCard.composition.forEach(item => {
-   const li = document.createElement("li");
-   li.textContent = item;
-   compositionList.appendChild(li);
-  }); 
+    const compositionList = productCardClone.querySelector(".composition-list");
+    productCard.composition.forEach(item => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      compositionList.appendChild(li);
+    }); 
 
-  container.appendChild(productCardClone);
+    container.appendChild(productCardClone);
   });
 }
 
 // 4. Задание: Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
- const productDescriptionsByReduce = productCards.reduce((acc,item) => {
+const productDescriptionsByReduce = productCards.reduce((acc,item) => {
   acc[item.title] = item.description;
   return acc;
 }, {});
@@ -37,10 +38,10 @@ console.log(productDescriptionsByReduce);
 
 // 5. Задание: Спрашиваем у пользователя число от 1 до 5 и показываем ровно столько карточек.
 function getCardCount() {
-  const input = prompt("Сколько карточек отобразить? От 1 до 5")
-  const cardAmount = input === null || input.trim() === "" ? NaN: Number(input);
+  const input = prompt("Сколько карточек отобразить? От 1 до 5");
+  const cardAmount = input === null || input.trim() === "" ? NaN : Number(input);
 
-  if(cardAmount >=1 && cardAmount <=5) {
+  if (cardAmount >=1 && cardAmount <=5) {
     return cardAmount;
   } else {
     alert("Ошибка! Введите число от 1 до 5");
@@ -53,7 +54,6 @@ const count = getCardCount();
 if(count !== null) {
   const cardsToDisplay = productCards.slice(0, count);
   renderCards(cardsToDisplay);
-} else {
 }  
 
 
